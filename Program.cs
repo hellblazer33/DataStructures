06623248d766ecab51029ea193926b6c8778e7b8
Pankaj Kumar <pk110495@gmail.com>
2022-02-07 10:56:49 +0530

    added binary search of word program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿String[] arr = { "contribute", "geeks", "ide", "practice" };
+String x = "ide";
+
+int result = BinarySearchClass.binarySearch(arr, x);
+
+if (result == 0)
+    Console.WriteLine("Element not present");
+else
+    Console.WriteLine("Element found at "
+                    + "index " + result);
